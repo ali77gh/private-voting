276cca3ff795ea647fbd9468ff1adea702fd3c7c
Ali Ghahremani <alighahremani1377@gmail.com>
2025-11-01 13:22:33 +0330

    feat: collect votes + avoid duplicate votes + some notes

--- src/PrivateVoting.sol
+++ src/PrivateVoting.sol
@@ -3,12 +3,21 @@ pragma solidity ^0.8.13;
 
 import {IPrivateVoting} from "./IPrivateVoting.sol";
 
+// Questions:
+//      merkle tree can be a mapping because it's mostly 0 and we just save ones
+
 contract PrivateVoting is IPrivateVoting {
     bool public isSignupActive = true;
 
+    // TODO an sparse merkle tree
+
+    string[] public votes;
+    mapping(uint256 => bool) alreadyVotedNullifiers;
+
     function signup(uint256 commitment) external {
         require(isSignupActive, "signup is not active anymore");
-        // TODO
+        // TODO Add commitment to my merkle tree
+        // A commitment is `MiMC7(secret, 1)`
     }
 
     function finalize() external {
@@ -22,6 +31,13 @@ contract PrivateVoting is IPrivateVoting {
         string memory voteValue,
         uint256 nullifier
     ) external {
-        // TODO
+        require(!isSignupActive, "signup not finalized yet");
+        require(!alreadyVotedNullifiers[nullifier], "you already voted");
+        // TODO Contract should prove same user already signed up with a commitment
+        // nullifier is `MiMC7(secret, 2)`
+        // Relation between MiMC7(secret, 1), MiMC7(secret, 2) prove and verify done with circuits
+
+        alreadyVotedNullifiers[nullifier] = true;
+        votes.push(voteValue);
     }
 }
