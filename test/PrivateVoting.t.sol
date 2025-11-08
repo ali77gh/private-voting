66113fb89f8dd5b8914976d2a433b9c41c60cd9d
Ali Ghahremani <alighahremani1377@gmail.com>
2025-11-08 17:15:37 +0330

    test: simple Private voting constructor in PrivateVotingTest

--- test/PrivateVoting.t.sol
+++ test/PrivateVoting.t.sol
@@ -3,12 +3,21 @@ pragma solidity ^0.8.13;
 
 import {Test} from "forge-std/Test.sol";
 import {PrivateVoting} from "../src/PrivateVoting.sol";
+import {IPoseidon} from "../src/IPoseidon.sol";
 
 contract PrivateVotingTest is Test {
     PrivateVoting public privateVoting;
 
     function setUp() public {
-        privateVoting = new PrivateVoting();
+        // poseidon setup
+        string memory poseidonHex = vm.readFile("bytecodes/poseidon.hex");
+        bytes memory poseidonBytes = vm.parseBytes(poseidonHex);
+        address poseidonAddress = address(0x1234);
+
+        vm.etch(poseidonAddress, poseidonBytes);
+        IPoseidon poseidon = IPoseidon(poseidonAddress);
+
+        privateVoting = new PrivateVoting(poseidon);
     }
 
     function test_() public {}
