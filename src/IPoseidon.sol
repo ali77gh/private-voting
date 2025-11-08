30547233e2e08de06a14ed08c0b4d8491943c096
Ali Ghahremani <alighahremani1377@gmail.com>
2025-11-08 17:15:04 +0330

    fix: poseidon interface (pure added)

--- src/IPoseidon.sol
+++ src/IPoseidon.sol
@@ -2,5 +2,5 @@
 pragma solidity ^0.8.30;
 
 interface IPoseidon {
-    function poseidon(uint256[2] memory input) external returns (uint256);
+    function poseidon(uint256[2] memory input) external pure returns (uint256);
 }
