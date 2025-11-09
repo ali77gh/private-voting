74753dd8e15d02367f796cc968901b098eb20a7d
Ali Ghahremani <alighahremani1377@gmail.com>
2025-11-09 17:04:00 +0330

    test: fake hash function injection and test for merkle tree.

--- test/PrivateVoting.t.sol
+++ test/PrivateVoting.t.sol
@@ -9,16 +9,70 @@ contract PrivateVotingTest is Test {
     PrivateVoting public privateVoting;
 
     function setUp() public {
-        // poseidon setup
-        string memory poseidonHex = vm.readFile("bytecodes/poseidon.hex");
-        bytes memory poseidonBytes = vm.parseBytes(poseidonHex);
-        address poseidonAddress = address(0x1234);
-
-        vm.etch(poseidonAddress, poseidonBytes);
-        IPoseidon poseidon = IPoseidon(poseidonAddress);
-
-        privateVoting = new PrivateVoting(poseidon);
+        privateVoting = new PrivateVoting(new FakeHash());
     }
 
-    function test_() public {}
+    function test_merkleTreeTestEven() public {
+        privateVoting.signup(1);
+        privateVoting.signup(10);
+        privateVoting.signup(100);
+        privateVoting.signup(1000);
+
+        // layer 0
+        assertEq(privateVoting.getOrDefault(0, 0), 1);
+        assertEq(privateVoting.getOrDefault(0, 1), 10);
+        assertEq(privateVoting.getOrDefault(0, 2), 100);
+        assertEq(privateVoting.getOrDefault(0, 3), 1000);
+        assertEq(privateVoting.getOrDefault(0, 4), 0); // out of range
+
+        // layer 1
+        assertEq(privateVoting.getOrDefault(1, 0), 11);
+        assertEq(privateVoting.getOrDefault(1, 1), 1100);
+        assertEq(privateVoting.getOrDefault(1, 2), 0); // out of range
+
+        // layer 2 (last layer)
+        assertEq(privateVoting.getOrDefault(2, 0), 1111); // this is actually root
+        assertEq(privateVoting.getOrDefault(1, 1), 0); // out of range
+
+        assertEq(privateVoting.getRootOfTree(), 1111);
+    }
+
+    function test_merkleTreeTestOdd() public {
+        privateVoting.signup(1);
+        privateVoting.signup(10);
+        privateVoting.signup(100);
+        privateVoting.signup(1000);
+        privateVoting.signup(10000);
+
+        // layer 0
+        assertEq(privateVoting.getOrDefault(0, 0), 1);
+        assertEq(privateVoting.getOrDefault(0, 1), 10);
+        assertEq(privateVoting.getOrDefault(0, 2), 100);
+        assertEq(privateVoting.getOrDefault(0, 3), 1000);
+        assertEq(privateVoting.getOrDefault(0, 4), 10000);
+        assertEq(privateVoting.getOrDefault(0, 5), 0); // out of range
+
+        // layer 1
+        assertEq(privateVoting.getOrDefault(1, 0), 11);
+        assertEq(privateVoting.getOrDefault(1, 1), 1100);
+        assertEq(privateVoting.getOrDefault(1, 2), 10000); // 10000 + 0
+        assertEq(privateVoting.getOrDefault(1, 3), 0); // out of range
+
+        // layer 2
+        assertEq(privateVoting.getOrDefault(2, 0), 1111);
+        assertEq(privateVoting.getOrDefault(2, 1), 10000);
+        assertEq(privateVoting.getOrDefault(2, 2), 0); // out of range
+
+        // layer 3 (last layer)
+        assertEq(privateVoting.getOrDefault(2, 0), 11111);
+        assertEq(privateVoting.getOrDefault(2, 1), 0); // out of range
+
+        assertEq(privateVoting.getRootOfTree(), 11111);
+    }
+}
+
+contract FakeHash is IPoseidon {
+    function poseidon(uint256[2] memory input) external pure returns (uint256) {
+        return input[0] + input[1];
+    }
 }
